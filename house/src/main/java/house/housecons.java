948a395629e95f6ce32b56673d810e35d1be0695
crazy laptop <crazy laptop@SBI>
2020-02-19 20:22:36 +0530

    third commit

---
+++ house/src/main/java/house/housecons.java
@@ -0,0 +1,21 @@
+package house;
+import java.util.Scanner;
+
+public class housecons {
+	public static void main(String args[])
+	{
+		
+	}
+
+}
+class Hh{
+	int total,mat_std,total_area,Type;
+	Scanner sc=new Scanner(System.in);
+	{
+		mat_std=sc.nextInt();
+		total_area=sc.nextInt();
+	    Type=sc.nextInt();
+	    total=total_area*Type;
+	    sc.close();
+	}
+}
