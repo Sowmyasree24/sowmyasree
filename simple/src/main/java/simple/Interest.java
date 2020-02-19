342c3517e8911db5087b0308dd7c60f83bd6d2f2
crazy laptop <crazy laptop@SBI>
2020-02-19 19:54:58 +0530

    second commit

---
+++ simple/src/main/java/simple/Interest.java
@@ -0,0 +1,24 @@
+package simple;
+import java.util.Scanner;
+class a
+{
+	Double pr,roi,sim,comp,time;
+	Scanner sc=new Scanner(System.in);
+	{
+		pr=sc.nextDouble();
+		roi=sc.nextDouble();
+		time=sc.nextDouble();
+		sim=(pr*time*roi)/100;
+		comp=pr;
+		sc.close();
+		
+	}
+}
+
+public class Interest {
+	public static void main(String args[])
+	{
+		
+	}
+
+}
